e67a2634245bcc57cb625c8716cac1515a46b1b0
klanetho2003 <klanetho2003@users.noreply.github.com>
2025-02-20 14:34:43 +0900

    --< Code >-- KnockBack Debuff 끝 자락에 순간 이동하는 버그 수정 - 이로써 KnockBack Debuff 완 범위 판정 완 ready for FireBuster - sprite default 크기 조정

--- MagicSchool/Assets/@Scripts/Contents/Effect/CC/KnockBack.cs
+++ MagicSchool/Assets/@Scripts/Contents/Effect/CC/KnockBack.cs
@@ -42,8 +42,6 @@ public class KnockBack : CCBase
             yield return null;
         }
 
-        Owner.SetCellPos(Managers.Map.World2Cell(Owner.CellPos), true);
-
         // 넉백 상태가 끝난 후 상태 복귀
         if (Owner.CreatureState == CreatureState.Dameged)
             Owner.CreatureState = CreatureState.Idle;
